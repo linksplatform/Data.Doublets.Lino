f6f1eae875966c5f76aad87acf74b42eb2fe7440
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-14 13:52:10 +0600

    Update `LinoImporterCli`

--- csharp/Platform.Data.Doublets.Lino/LinoImporterCli.cs
+++ csharp/Platform.Data.Doublets.Lino/LinoImporterCli.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Platform.Data.Doublets.Memory.United.Generic;
 using Platform.IO;
 using Platform.Memory;
@@ -9,11 +10,13 @@ public class LinoImporterCli<TLinkAddress>
     public void Run(params string[] args)
     {
         var argumentsIndex = 0;
-        // var notationFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex++, "Path to a notation file", args);
-        // var storageFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex, "Path to a links storage", args);
-        // var linksMemory = new FileMappedResizableDirectMemory(storageFilePath);
-        // var links = new UnitedMemoryLinks<TLinkAddress>(linksMemory).DecorateWithAutomaticUniquenessAndUsagesResolution();
-        // var linoStorage = new DefaultLinoStorage<TLinkAddress>();
-        // var importer = new LinoImporter<TLinkAddress>()
+        var notationFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex++, "A path to a notation file", args);
+        var storageFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex, "A path to a links storage", args);
+        var linksMemory = new FileMappedResizableDirectMemory(storageFilePath);
+        var links = new UnitedMemoryLinks<TLinkAddress>(linksMemory).DecorateWithAutomaticUniquenessAndUsagesResolution();
+        var linoStorage = new DefaultLinoStorage<TLinkAddress>(links);
+        var importer = new LinoImporter<TLinkAddress>(linoStorage);
+        var notation = File.ReadAllText(notationFilePath);
+        importer.Import(notation);
     }
 }
