cc881a30ecc3258be1f69a8847fa1e61f80f04c6
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-14 13:53:30 +0600

    Add Exporter Cli tool

---
+++ csharp/Platform.Data.Doublets.Lino.Exporter/Program.cs
@@ -0,0 +1,22 @@
+﻿using TLinkAddress = System.UInt64;
+
+namespace Platform.Data.Doublets.Lino.Exporter
+{
+    class Program
+    {
+        /// <summary>
+        /// <para>
+        /// Main the args.
+        /// </para>
+        /// <para></para>
+        /// </summary>
+        /// <param name="args">
+        /// <para>The args.</para>
+        /// <para></para>
+        /// </param>
+        static void Main(params string[] args)
+        {
+            new LinoExporterCli<TLinkAddress>().Run(args);
+        }
+    }
+}
