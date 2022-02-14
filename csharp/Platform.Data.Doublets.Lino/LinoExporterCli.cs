801ee059b23aa1746a087caeb7661665c591cbe0
FreePhoenix888 <freephoenix888@gmail.com>
2022-02-14 15:47:37 +0600

    Add `TLinkAddress` constraint in `Lino(Importer|Exporter)Cli`

--- csharp/Platform.Data.Doublets.Lino/LinoExporterCli.cs
+++ csharp/Platform.Data.Doublets.Lino/LinoExporterCli.cs
@@ -1,20 +1,30 @@
 using System.IO;
 using Platform.Data.Doublets.Memory.United.Generic;
+using Platform.Data.Doublets.Sequences.Converters;
 using Platform.IO;
 using Platform.Memory;
 
 namespace Platform.Data.Doublets.Lino;
 
-public class LinoExporterCli<TLinkAddress>
+public class LinoExporterCli<TLinkAddress> where TLinkAddress : struct
 {
     public void Run(params string[] args)
     {
         var argumentsIndex = 0;
-        var storageFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex, "A path to a links storage", args);
-        var notationFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex, "A path to a notation file", args);
+        var storageFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex++, "A path to a links storage", args);
+        var notationFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex++, "A path to a notation file", args);
+        var documentName = ConsoleHelpers.GetOrReadArgument(argumentsIndex++, "A document name", args);
         using var linksMemory = new FileMappedResizableDirectMemory(storageFilePath);
-        var links = new UnitedMemoryLinks<TLinkAddress>(linksMemory).DecorateWithAutomaticUniquenessAndUsagesResolution();
-        var linoStorage = new DefaultLinoStorage<TLinkAddress>(links);
+        var storage = new UnitedMemoryLinks<TLinkAddress>(linksMemory).DecorateWithAutomaticUniquenessAndUsagesResolution();
+        ILinoStorage<TLinkAddress> linoStorage;
+        if (string.IsNullOrWhiteSpace(documentName))
+        {
+            linoStorage = new DefaultLinoStorage<TLinkAddress>(storage);
+        }
+        else
+        {
+            linoStorage = new LinoDocumentsStorage<TLinkAddress>(storage, new BalancedVariantConverter<TLinkAddress>(storage));
+        }
         var exporter = new LinoExporter<TLinkAddress>(linoStorage);
         var allLinksNotation = exporter.GetAllLinks();
         File.WriteAllText(notationFilePath, allLinksNotation);
--- csharp/Platform.Data.Doublets.Lino/LinoImporterCli.cs
+++ csharp/Platform.Data.Doublets.Lino/LinoImporterCli.cs
@@ -1,20 +1,30 @@
 using System.IO;
 using Platform.Data.Doublets.Memory.United.Generic;
+using Platform.Data.Doublets.Sequences.Converters;
 using Platform.IO;
 using Platform.Memory;
 
 namespace Platform.Data.Doublets.Lino;
 
-public class LinoImporterCli<TLinkAddress>
+public class LinoImporterCli<TLinkAddress> where TLinkAddress : struct
 {
     public void Run(params string[] args)
     {
         var argumentsIndex = 0;
         var notationFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex++, "A path to a notation file", args);
-        var storageFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex, "A path to a links storage", args);
+        var storageFilePath = ConsoleHelpers.GetOrReadArgument(argumentsIndex++, "A path to a links storage", args);
+        var documentName = ConsoleHelpers.GetOrReadArgument(argumentsIndex++, "A document name", args);
         using var linksMemory = new FileMappedResizableDirectMemory(storageFilePath);
-        var links = new UnitedMemoryLinks<TLinkAddress>(linksMemory).DecorateWithAutomaticUniquenessAndUsagesResolution();
-        var linoStorage = new DefaultLinoStorage<TLinkAddress>(links);
+        var storage = new UnitedMemoryLinks<TLinkAddress>(linksMemory).DecorateWithAutomaticUniquenessAndUsagesResolution();
+        ILinoStorage<TLinkAddress> linoStorage;
+        if (string.IsNullOrWhiteSpace(documentName))
+        {
+            linoStorage = new DefaultLinoStorage<TLinkAddress>(storage);
+        }
+        else
+        {
+            linoStorage = new LinoDocumentsStorage<TLinkAddress>(storage, new BalancedVariantConverter<TLinkAddress>(storage));
+        }
         var importer = new LinoImporter<TLinkAddress>(linoStorage);
         var notation = File.ReadAllText(notationFilePath);
         importer.Import(notation);
